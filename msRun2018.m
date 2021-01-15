f155bda44dda7025aea54305b5d9b57c7f4da699
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2021-01-15 13:52:56 -0500

    Update msRun2018.m

--- msRun2018.m
+++ msRun2018.m
@@ -14,7 +14,6 @@
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 3 of the License, or any
 % later version.
-% Contact: etterguillaume@gmail.com
 
 %% Auto-detect operating system
 if ispc
