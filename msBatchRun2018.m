cc72a4435776f67d3a18defcb424b5251d9bc5da
Guillaume Etter <35970969+etterguillaume@users.noreply.github.com>
2021-01-15 13:53:21 -0500

    Update msBatchRun2018.m

--- msBatchRun2018.m
+++ msBatchRun2018.m
@@ -17,7 +17,6 @@
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 3 of the License, or any
 % later version.
-% Contact: etterguillaume@gmail.com
 
 %% Auto-detect operating system
 if ispc
